53f8ba6dc03e26dad25bf9a196e28d0f55f0bc9a
SupavichSpu <supavich.chu@spumail.net>
2025-11-15 23:51:26 +0700

    intital com

---
+++ src/components/AppFooter.jsx
@@ -0,0 +1,10 @@
+const AppFooter = () => {
+    return (<>
+        <center>
+            <h4 className="m-4">IG: nong_furemu</h4>
+        </center>
+    </>)
+        ;
+}
+
+export default AppFooter;
